aee066b40a0ee4c9e24baaddaee8976301bda1e4
Andrei Galitianu <andrei.galitianu@ciconia-systems.com>
2024-01-13 18:13:04 +0200

    implemented decoder with skip connections

--- model/UNet.py
+++ model/UNet.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms as transforms
 
 
-class Block(nn.Module):
+class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(Block, self).__init__()
+        super(EncoderBlock, self).__init__()
         self.layers = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -19,12 +20,34 @@ class Block(nn.Module):
         return self.layers(x)
 
 
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.up = nn.ConvTranspose2d(in_channels, middle_channels, kernel_size=2, stride=2)
+        self.bn = nn.BatchNorm2d(middle_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv_block = EncoderBlock(middle_channels + out_channels, out_channels)
+
+    def forward(self, x, skip):
+        x = self.up(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        # Crop the skip connection and concatenate
+        crop_size = x.size()[2:]
+        crop = transforms.CenterCrop(crop_size)(skip)
+        x = torch.cat([x, crop], dim=1)
+
+        x = self.conv_block(x)
+        return x
+
+
 class Encoder(nn.Module):
     def __init__(self, nrChannelsPerBlock=[3, 64, 128, 256, 512, 1024]):
         super(Encoder, self).__init__()
         self.layers = nn.ModuleList()
         for block in range(len(nrChannelsPerBlock) - 1):
-            self.layers.append(Block(nrChannelsPerBlock[block], nrChannelsPerBlock[block + 1]))
+            self.layers.append(EncoderBlock(nrChannelsPerBlock[block], nrChannelsPerBlock[block + 1]))
             # self.layers.append(nn.MaxPool2d(2))
 
     def forward(self, x):
@@ -37,7 +60,19 @@ class Encoder(nn.Module):
         return skip_connections
 
 
+class Decoder(nn.Module):
+    def __init__(self, depths):
+        super(Decoder, self).__init__()
+        self.layers = nn.ModuleList()
+        for i in range(len(depths) - 2, -1, -1):
+            self.layers.append(DecoderBlock(depths[i + 1], depths[i], depths[i]))
+
+    def forward(self, x, enc_activations):
+        for i, layer in enumerate(self.layers):
+            x = layer(x, enc_activations[i])
+        return x
+
+
 class UNet(torch.nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-
